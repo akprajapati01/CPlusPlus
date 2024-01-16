1c5069eba6946a649b550c15a821fa179d59aba2
akprajapati01 <ap312863@gmail.com>
2024-01-16 10:19:26 +0530

    Files Updated

---
+++ Table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+    int n ,i;
+    std::cin>>n;
+    std::cout<<"Hello world";
+    std::cout<<"Multiplicaton Table"<<n;
+    for(i=1;i<=10;i++)
+    {
+        std::cout<<(n*i);
+    }
+
+
+}
