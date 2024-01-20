76d3d9d18bf9e2f91575af4a6d50d7421def8c7a
akprajapati01 <ap312863@gmail.com>
2024-01-20 14:57:31 +0530

    Files Added

---
+++ vote.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Input Your Age: ";
+    cin>>n;
+    if(n>=18)
+        {
+            cout<<"You can vote";
+        }
+    else
+        {
+        cout<<"You cannot vote";
+        }
+}
