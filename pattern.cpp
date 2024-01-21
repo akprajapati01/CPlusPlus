a929aae4e996523841317ee667407c5d9a8518f5
akprajapati01 <ap312863@gmail.com>
2024-01-21 14:52:21 +0530

    Pattern Files Added

---
+++ pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"Input any Number: ";
+    cin>>n;
+    for(i=0;i<=n;i++){
+        for(j=i;j<=n;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
