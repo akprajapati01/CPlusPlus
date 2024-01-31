673fb4f73a1c49f3f3cfa0131b3ffaddaa47ec75
akprajapati01 <ap312863@gmail.com>
2024-01-31 10:08:00 +0530

    Files Added

---
+++ Variable-Comment.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    float x,y,z;
+    a=7;
+    cout<<"Hello World"<<" "<<a;
+    // single line comment
+    /* multi line comment*/
+    return 0;
+}
