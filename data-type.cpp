f775f0cf5e99f462e61c7e0e01a07419215e7c6f
akprajapati01 <ap312863@gmail.com>
2024-02-09 18:50:26 +0530

    File Updated

---
+++ data-type.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    char c;
+    double d;
+    bool e;
+    a=5;
+    b=2.39266286;
+    c='a';
+    d=85.2794394367;
+    e=true;
+    cout<<a;
+    cout<<"\n"<<b;
+    cout<<"\n"<<c;
+    cout<<"\n"<<d;
+    cout<<"\n"<<e;
+    cout<<"End\n";
+
+    return 0;
+}
