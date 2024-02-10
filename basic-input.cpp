2ed25c72e4abafec74432452ca355c380e5b9f55
akprajapati01 <ap312863@gmail.com>
2024-02-10 19:35:55 +0530

    File Addded

---
+++ basic-input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Input First Number:";
+    cin>>num1;
+    cout<<"Input Second Number:";
+    cin>>num2;
+    cout<<"Sum of Two number = "<<num1+num2;
+
+} 
