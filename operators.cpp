7eb475a8f33e249f595754aba8ce88289690b18c
akprajapati01 <ap312863@gmail.com>
2024-02-11 11:01:31 +0530

    File Addded

---
+++ operators.cpp
@@ -0,0 +1,9 @@
+// System Header File
+#include<iostream>
+// User Defined Header file
+// #include "File.h"
+using namespace std;
+int main(){
+    cout<<"HEllo Programmer";
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-#include<iostream>
-int main(){
-    int a=6,b=7;
-    float c;
-    c=a*b;
-    std::cout<<"Multiplication:"<<c;
+System Header File
