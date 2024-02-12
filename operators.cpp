68527120326d210d7099b4c7c858f818d15d24b2
akprajapati01 <ap312863@gmail.com>
2024-02-12 10:54:05 +0530

    File Modified

--- operators.cpp
+++ operators.cpp
@@ -4,6 +4,17 @@
 // #include "File.h"
 using namespace std;
 int main(){
+    int a=7,b=15;
     cout<<"HEllo Programmer";
+    cout<<"Operators in C++";
+    cout<<"The value of a+b = "<<a+b<<endl;
+    cout<<"The value of a-b = "<<a-b<<endl;
+    cout<<"The value of a*b = "<<a*b<<endl;
+    cout<<"The value of a/b = "<<a/b<<endl;
+    cout<<"The value of a%b = "<<a%b<<endl;
+    cout<<"The value of a++ = "<<a++<<endl;
+    cout<<"The value of a-- = "<<a--<<endl;
+    cout<<"The value of --a = "<<--a<<endl;
+    cout<<"The value of ++a = "<<++a<<endl;
     return 0;
 }
