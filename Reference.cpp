117c8dbac07775a0c8a4c06c9991d22a89efde85
akprajapati01 <ap312863@gmail.com>
2024-02-14 14:36:17 +0530

    File Added

---
+++ Reference.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int c=69;
+int main(){
+    int a,b,c;
+    cout<<"Input The Value of a:"<<endl;
+    cin>>a;
+    cout<<"Input The Value of b:"<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"The Sum of a + b = "<<c<<endl;
+    cout<<"The value of global c :"<<::c;
+    return 0;
+}
