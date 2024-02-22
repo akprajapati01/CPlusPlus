0c033177e86680718b602a1c1f491ce5cf394690
akprajapati01 <ap312863@gmail.com>
2024-02-22 21:28:52 +0530

    Files Added

---
+++ Constant.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=34;
+    cout<<"The Value of a was : "<<a<<endl;
+    a=98;
+    cout<<"The Value of a is : "<<a<<endl;
+    // Constant in C++
+    const int b=10;
+    cout<<"The Value of b is : "<<b<<endl;
+    return 0;
+
+}
