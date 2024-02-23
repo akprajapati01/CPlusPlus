b61a41dcdfe5ffcc612235396be9489ce6d02059
akprajapati01 <ap312863@gmail.com>
2024-02-23 20:59:20 +0530

    Files Modified

--- Constant.cpp
+++ Constant.cpp
@@ -4,10 +4,10 @@ int main(){
     int a=34;
     cout<<"The Value of a was : "<<a<<endl;
     a=98;
-    cout<<"The Value of a is : "<<a<<endl;
+    cout<<"The Value of a is : "<<a<<endl; //Here the value of a gets updated
     // Constant in C++
     const int b=10;
-    cout<<"The Value of b is : "<<b<<endl;
+    cout<<"The Value of b is : "<<b<<endl; //Here the value of b did not gets updated because b is constant value
     return 0;
 
 }
