ac5d9414ed4114215e656b23279ff34cf9a8b9bc
akprajapati01 <ap312863@gmail.com>
2024-02-26 11:09:03 +0530

    Files Modified

--- Moderator.cpp
+++ Moderator.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int main(){
     int a=4,b=68,c=796;
+
+    //Moderator
     cout<<"The Value of a is :"<<a<<endl;
     cout<<"The Value of a is :"<<b<<endl;
     cout<<"The Value of a is :"<<c<<endl;
