176d5f11af2f9ba034a4c6b7bc5cc47e015b8d41
akprajapati01 <ap312863@gmail.com>
2024-02-26 11:01:24 +0530

    Files Modified

--- Moderator.cpp
+++ Moderator.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main(){
+    int a=4,b=68,c=796;
+    cout<<"The Value of a is :"<<a<<endl;
+    cout<<"The Value of a is :"<<b<<endl;
+    cout<<"The Value of a is :"<<c<<endl;
+
+    cout<<"The Value of a is setw :"<<setw(6)<<a<<endl;
+    cout<<"The Value of a is setw :"<<setw(6)<<b<<endl;
+    cout<<"The Value of a is setw :"<<setw(6)<<c<<endl;
 
     return 0;
 }
