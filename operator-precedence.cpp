4f8f4926c4c4c517b341164713f359d01b1087e9
akprajapati01 <ap312863@gmail.com>
2024-02-29 21:10:15 +0530

    File Added

--- operator-precedence.cpp
+++ operator-precedence.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=5 b=7,c;
-    c=23/68+32*2;
-    cout<<c;
+    int a=5, b=7;
+    float c,d;
+    c=2*23/68*32+2;
+    cout<<c<<endl;
+    //Here All operation takes place according to operator precedence
+    d=2*(23/68)*(32+2);
+    cout<<d;
 
     return 0;
 }
