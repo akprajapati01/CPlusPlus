992fc249ff4eccbeaed267781bebcccc3f9f94eb
akprajapati01 <ap312863@gmail.com>
2024-03-09 10:51:55 +0530

    Files Modified

--- loop-str.cpp
+++ loop-str.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 int main(){
     int i;
-    for(i=0;i<5;i++)   //This is Simple loop statement
+    for(i=0;i<5;i++)   //This is Simple For loop statement
     {
         cout<<i<<" ";
+    }
+    cout<<endl;
+    int j=2;
+    while(j<10)
+    {
+        cout<<j<<" ";
+        j++;
     }
     return 0;
 }
