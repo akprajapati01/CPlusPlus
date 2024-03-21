61d092e6f91dedd6aca3ee9eed50d970ba410439
akprajapati01 <ap312863@gmail.com>
2024-03-21 08:15:52 +0530

    Files Modified and Added

--- day-switch.cpp
+++ day-switch.cpp
@@ -9,6 +9,36 @@ int main(){
             cout<<"Monday";
             break;
         }
+        case 2:
+        {
+            cout<<"Tuesday";
+            break;
+        } 
+        case 3:
+        {
+            cout<<"Wednsday";
+            break;
+        }
+        case 4:
+        {
+            cout<<"Thrusday";
+            break;
+        }
+         case 5:
+        {
+            cout<<"Friday";
+            break;
+        }
+         case 6:
+        {
+            cout<<"Saturday";
+            break;
+        }
+         case 7:
+        {
+            cout<<"Sunday";
+            break;
+        }
         
     }
     return 0;
