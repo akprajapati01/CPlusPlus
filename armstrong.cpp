94143bea15dbea082accb8ad398c2b114bf82956
akprajapati01 <ap312863@gmail.com>
2024-03-25 11:07:10 +0530

    Files Modified

--- armstrong.cpp
+++ armstrong.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
-    int num,i;
-    cout<<"Input any number :";
-    cin>>num;
-    
+int num,i,a,s,check,ch,sum;
+cout<<"Input any number :";
+cin>>num;
+check=num;
+while (num>0)
+    {
+        s=num%10;
+        ch=pow(s,3);
+        sum=sum+ch;
+
+    }
+    a=num/10;
+if(check==sum)
+{
+    cout<<"Armstrong number";
+}
+else{
+    cout<<"Not Armstrong Number";
+}
     return 0;
 }
