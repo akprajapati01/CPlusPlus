1371d155ad3c744710c34e6675a6ec9a47515dbf
akprajapati01 <ap312863@gmail.com>
2024-03-28 12:00:54 +0530

    File Modified

--- fibonnaci.cpp
+++ fibonnaci.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,j;
+    int i,j,sum=0;
     cout<<"Input Number of terms :";
     cin>>i;
-    
+    for(j=0;j<=i;j++){
+        
+    }
     return 0;
 }
